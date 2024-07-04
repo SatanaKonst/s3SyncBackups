9b80ea53a621425609f22280368ecae1245325dc
Konstantin <Satana.Konst@gmail.com>
2024-07-04 13:57:18 +0300

    Изменен метод отправки сообщений в Telegram Get заменен на пост т.к размер сообщения вырос

--- functions.py
+++ functions.py
@@ -1,3 +1,4 @@
+import json
 import re
 import subprocess
 import requests
@@ -113,6 +114,16 @@ def clearRemoteBackups(BACKUP_SAVE_COUNT, REMOTE_NAME, BACKUP_CONTAINER_NAME):
 
 
 def telegram_bot_sendtext(bot_token, bot_chatID, bot_message):
-    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
-    response = requests.get(send_text)
+    url = 'https://api.telegram.org/bot' + bot_token + '/sendMessage'
+    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+    data = {
+        "chat_id": bot_chatID,
+        "parse_mode": "Markdown",
+        "text": bot_message
+    }
+    response = requests.post(
+        url,
+        data=json.dumps(data),
+        headers=headers
+    )
     return response.json()
